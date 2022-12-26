cc129b267f6a8c96ff432309239ba07816f77589
alifshah <alifhasanshah@gmail.com>
2022-12-26 10:45:33 +0600

    tailwind install and configuration complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <>
-     <h1>Dorex foodhall boilerplate created</h1>
+     <h1 className='text-3xl font-bold text-center mt-5 '>Dorex foodhall boilerplate created</h1>
     </>
   );
 }
